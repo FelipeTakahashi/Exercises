9438f222d62dfbe7862800d977805c997d84f858
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-06-03 18:28:52 -0300

    Water Consumption

---
+++ CodeChef/waterconsumption.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int t, x;
+        std::cin >> t;
+        
+    for (int i = 0; i < t; ++i) {
+        std::cin >> x;
+        if (x >= 2000) {
+            std::cout << "YES" << std::endl;
+        }
+        else {
+            std::cout << "NO" << std::endl;
+        }
+    }
+}
