530f11c2720d5e641cfe8b388aee4494e3803f79
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-06-03 18:12:39 -0300

    Add Two Numbers

---
+++ CodeChef/addtwonumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, reps;
+    
+        cin >> reps;
+        
+    for (int i = 0; i < reps; ++i) {
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+}
