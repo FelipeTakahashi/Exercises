cee75f1d466a3a42569893240e929545a8b656b0
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-06-03 18:13:32 -0300

    Squats

---
+++ CodeChef/squats.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int t, x;
+        std::cin >> t;
+        
+    for (int i = 0; i < t; ++i) {
+        std::cin >> x;
+        std::cout << x * 15 << std::endl;
+    }
+}
