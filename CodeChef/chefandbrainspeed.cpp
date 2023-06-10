63c2c9d731ac0e735d3f6f5c7dc3dbd29b628210
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-06-10 19:17:22 -0300

    Chef And Brain Speed

---
+++ CodeChef/chefandbrainspeed.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+        std::cin >> a >> b;
+        
+    if (a < b) std::cout << "YES" << std::endl;
+    else std::cout << "NO" << std::endl;
+}
